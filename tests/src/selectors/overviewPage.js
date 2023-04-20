5172202a56c09776d492b124c1ac35ca94dc4118
tongilcoto <tongilcoto@gmail.com>
2023-04-20 08:15:55 +0200

    SDPC-59 Open left menu and SDPC-60 All items

--- tests/src/selectors/overviewPage.js
+++ tests/src/selectors/overviewPage.js
@@ -2,5 +2,6 @@ exports.title = '.title';
 exports.pageOptions = {
     "Cancel": '#cancel',
     "Finish": '#finish',
+    "Menu": '#react-burger-menu-btn',
     "Shopping Cart": '.shopping_cart_link'
 }
